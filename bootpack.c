56bb4990822f1abebf2cc2d7eb362dd3d68a310a
morix1500 <rade1500@gmail.com>
2019-06-22 08:37:28 +0900

    day4 4

--- bootpack.c
+++ bootpack.c
@@ -4,10 +4,10 @@ void io_hlt(void);
 void HariMain(void) {
 	int i;
 	char *p; // メモリ専用番地と宣言。charは1バイトだがメモリ番地を指定するので4バイト保持する
+	p = (char *) 0xa0000;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		p = (char *) i; // 番地を代入。 MOV ECX, i
-		*p = i & 0x0f;  // MOV BYTE [ECX], (i & 0x0f) がやりたい
+	for (i = 0; i <= 0xaffff; i++) {
+		*(p + i) = i & 0x0f;  // MOV BYTE [ECX], (i & 0x0f) がやりたい
 	}
 	for (;;) {
 		io_hlt();
