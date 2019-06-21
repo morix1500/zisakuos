16071a57e6fc8fa563d138f29d08c4597bb6e21a
morix1500 <rade1500@gmail.com>
2019-06-22 08:35:44 +0900

    day4 3

--- bootpack.c
+++ bootpack.c
@@ -1,11 +1,13 @@
 // {}がなくていきなり;を書くと、他のファイルにある という意味になる
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void) {
 	int i;
+	char *p; // メモリ専用番地と宣言。charは1バイトだがメモリ番地を指定するので4バイト保持する
+
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f);
+		p = (char *) i; // 番地を代入。 MOV ECX, i
+		*p = i & 0x0f;  // MOV BYTE [ECX], (i & 0x0f) がやりたい
 	}
 	for (;;) {
 		io_hlt();
