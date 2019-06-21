8bc2e70568a3e0eab4532bb8f530eac9d91b4ab8
morix1500 <rade1500@gmail.com>
2019-06-22 08:17:29 +0900

    day4 2

--- bootpack.c
+++ bootpack.c
@@ -5,7 +5,7 @@ void write_mem8(int addr, int data);
 void HariMain(void) {
 	int i;
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, 14);
+		write_mem8(i, i & 0x0f);
 	}
 	for (;;) {
 		io_hlt();
