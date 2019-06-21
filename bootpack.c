16209a422bd8639dfa9f0ed52348089b0cf0c860
morix1500 <rade1500@gmail.com>
2019-06-22 08:11:58 +0900

    day4 1

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,13 @@
 // {}がなくていきなり;を書くと、他のファイルにある という意味になる
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void) {
-fin:
-	io_hlt();
-	goto fin;
+	int i;
+	for (i = 0xa0000; i <= 0xaffff; i++) {
+		write_mem8(i, 14);
+	}
+	for (;;) {
+		io_hlt();
+	}
 }
