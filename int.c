e575318b4a0d14f7ae20bc1fdf0337243cb16c38
morix1500 <rade1500@gmail.com>
2019-06-23 18:18:48 +0900

    day7 1

--- int.c
+++ int.c
@@ -21,15 +21,20 @@ void init_pic(void)
 	return;
 }
 
+#define PORT_KEYDAT 0x0060
+
 // PS/2キーボードからの割り込み
 void inthandler21(int *esp) {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-	putfont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 21 (IRQ-1) : PS/2 keyboard");
+	unsigned char data, s[4];
+	io_out8(PIC0_OCW2, 0x61); // IRQ-01受付完了をPICに通知。 01100001
+	data = io_in8(PORT_KEYDAT);
 
-	for (;;) {
-		io_hlt();
-	}
+	sprintf(s, "%02X", data);
+	boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+	putfont8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+
+	return;
 }
 
 // PS/2マウスからの割り込み
