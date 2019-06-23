ce7fe39f2d65e0521e4d5c1fb4ef42c0f2a70fa4
morix1500 <rade1500@gmail.com>
2019-06-23 19:31:26 +0900

    day7 5

--- int.c
+++ int.c
@@ -22,21 +22,14 @@ void init_pic(void)
 }
 
 #define PORT_KEYDAT 0x0060
-struct KEYBUF keybuf;
+struct FIFO8 keyfifo;
 
 // PS/2キーボードからの割り込み
 void inthandler21(int *esp) {
 	unsigned char data;
 	io_out8(PIC0_OCW2, 0x61); // IRQ-01受付完了をPICに通知。 01100001
 	data = io_in8(PORT_KEYDAT);
-	if (keybuf.len < 32) {
-		keybuf.data[keybuf.next_w] = data;
-		keybuf.len++;
-		keybuf.next_w++;
-		if (keybuf.next_w == 32) {
-			keybuf.next_w = 0;
-		}
-	}
+	fifo8_put(&keyfifo, data);
 
 	return;
 }
