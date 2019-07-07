702c179d065adf4434e1b019f29e6b628a1856b9
morix1500 <rade1500@gmail.com>
2019-07-07 10:10:36 +0900

    day18 3

--- bootpack.c
+++ bootpack.c
@@ -183,6 +183,11 @@ void HariMain(void) {
 					}
 
 				}
+				if (i == 256 + 0x1c) { // Enter
+					if (key_to != 0) { // コンソールへ
+						fifo32_put(&task_cons->fifo, 10 + 256);
+					}
+				}
 				if (i == 256 + 0x0f) {// Tab
 					if (key_to == 0) {
 						key_to = 1;
@@ -382,7 +387,7 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
 void console_task(struct SHEET *sheet) {
 	struct TIMER *timer;
 	struct TASK *task = task_now();
-	int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
+	int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
 	char s[2];
 
 	fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -419,7 +424,7 @@ void console_task(struct SHEET *sheet) {
 				cursor_c = COL8_FFFFFF;
 			}
 			if (i == 3) { // カーソルOFF
-				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, 28, cursor_x + 7, 43);
+				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
 				cursor_c = -1;
 			}
 			if (256 <= i && i <= 511) { // キーボードデータ(タスクA経由)
@@ -427,8 +432,19 @@ void console_task(struct SHEET *sheet) {
 					// バックスペース
 					if (cursor_x > 16) {
 						// カーソルをスペースで消してからカーソルを１つ戻す
-						putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, " ", 1);
+						putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
 						cursor_x -= 8;
+					}
+				} else if (i == 10 + 256) {
+					// Enter
+					if (cursor_y < 28 + 112) {
+						// カーソルをスペースで消す
+						putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+						cursor_y += 16;
+						// プロンプト表示
+						putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">", 1);
+						cursor_x = 16;
+
 					}
 				} else {
 					// 一般文字
@@ -436,7 +452,7 @@ void console_task(struct SHEET *sheet) {
 						// １文字表示してからカーソルを１つすすめる
 						s[0] = i - 256;
 						s[1] = 0;
-						putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, s, 1);
+						putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
 						cursor_x += 8;
 
 					}
@@ -444,9 +460,9 @@ void console_task(struct SHEET *sheet) {
 			}
 			// カーソル再表示
 			if (cursor_c >= 0) {
-				boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+				boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
 			}
-			sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+			sheet_refresh(sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
 		}
 	}
 }
