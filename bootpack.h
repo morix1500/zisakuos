ab8d1f12c6698f279f8e8221b96e1c9d93bbfd05
morix1500 <rade1500@gmail.com>
2019-07-10 22:05:22 +0900

    day21 5

--- bootpack.h
+++ bootpack.h
@@ -24,6 +24,7 @@ void load_idtr(int limit, int addr);
 int  load_cr0(void);
 void load_tr(int tr);
 void store_cr0(int cr0);
+void asm_inthandler0d(void);
 void asm_inthandler20(void);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
@@ -255,6 +256,7 @@ void cmd_cat(struct CONSOLE *cons, int *fat, char *cmdline);
 void cmd_hlt(struct CONSOLE *cons, int *fat);
 int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 void hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
+int inthandler0d(int *esp);
 
 // file.c
 struct FILEINFO {
