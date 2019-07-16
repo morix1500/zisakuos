5857dfb786d36aa0cb0389fe71a3c447405e9347
morix1500 <rade1500@gmail.com>
2019-07-16 19:08:38 +0900

    day26 2

--- bootpack.c
+++ bootpack.c
@@ -40,7 +40,7 @@ void HariMain(void) {
 		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
 	};
 	int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
-	int j, x, y, mmx = -1, mmy = -1;
+	int j, x, y, mmx = -1, mmy = -1, mmx2 = 0;
 	struct SHEET *sht = 0, *key_win;
 
 	init_gdtidt();
@@ -247,6 +247,7 @@ void HariMain(void) {
 											// ウィンドウ移動モードへ
 											mmx = mx;
 											mmy = my;
+											mmx2 = sht->vx0;
 										}
 										if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
 											// xボタンクリック
@@ -267,9 +268,8 @@ void HariMain(void) {
 							// ウィンドウ移動モードの場合
 							x = mx - mmx; // マウスの移動量を計算
 							y = my - mmy;
-							sheet_slide(sht, sht->vx0 + x, sht->vy0 + y);
-							mmx = mx; // 移動後の座標に更新
-							mmy = my;
+							sheet_slide(sht, (mmx2 + x + 2) & ~3, sht->vy0 + y);
+							mmy = my; // 移動後の座標に更新
 						}
 					} else {
 						// 左ボタンを押していない
